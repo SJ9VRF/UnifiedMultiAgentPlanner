2d1fd920a2bbaf58c537b91467cafb3b058dce03
Arefeh Yavary <yavary.rf@gmail.com>
2024-08-29 01:23:08 -0700

    Update actions.py

--- src/actions.py
+++ src/actions.py
@@ -1,1 +1,18 @@
 # Defines actions and their effects
+from unified_planning.model import Fluent, InstantaneousAction, UserType, Object
+
+class ActionSetup:
+    def __init__(self, problem):
+        self.problem = problem
+
+    def create_fluent(self, name, user_type=None):
+        fluent = Fluent(name, user_type=user_type)
+        return fluent
+
+    def create_action(self, name, preconditions, effects):
+        action = InstantaneousAction(name)
+        for precondition in preconditions:
+            action.add_precondition(precondition)
+        for effect, status in effects.items():
+            action.add_effect(effect, status)
+        return action
