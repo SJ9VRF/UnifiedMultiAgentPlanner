b9b746669e59cbcd6e569a7d9e457f7ea3891704
Arefeh Yavary <yavary.rf@gmail.com>
2024-08-29 01:22:30 -0700

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,6 @@
 # Makes src a Python package
 
+# src/__init__.py
+# This file makes 'src' a Python package so its modules can be imported elsewhere in the project.
+
+# You can also use this file to perform package-wide initializations or define global variables if needed.
