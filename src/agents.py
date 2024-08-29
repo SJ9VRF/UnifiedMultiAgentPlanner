d05f4573da2981b1384369e8c4c83b97a40515d0
Arefeh Yavary <yavary.rf@gmail.com>
2024-08-29 01:22:52 -0700

    Update agents.py

--- src/agents.py
+++ src/agents.py
@@ -1,1 +1,11 @@
 # Defines Agent and MultiAgentProblem classes
+from unified_planning.model import Agent, MultiAgentProblem
+
+class MultiAgentSetup:
+    def __init__(self, name):
+        self.problem = MultiAgentProblem(name)
+
+    def add_agent(self, agent_name):
+        agent = Agent(agent_name, self.problem)
+        self.problem.add_agent(agent)
+        return agent
