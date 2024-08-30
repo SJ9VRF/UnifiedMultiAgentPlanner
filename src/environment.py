c15a66fec14e1d8af4d80cadd2a78f7275e69fe0
Arefeh Yavary <yavary.rf@gmail.com>
2024-08-30 01:22:40 -0700

    Update environment.py

--- src/environment.py
+++ src/environment.py
@@ -1,1 +1,14 @@
 # Set up the environment, user types, and objects
+from unified_planning.model import UserType, Object
+
+class EnvironmentSetup:
+    def __init__(self, problem):
+        self.problem = problem
+
+    def add_user_type(self, name):
+        return UserType(name)
+
+    def add_object(self, name, user_type):
+        obj = Object(name, user_type)
+        self.problem.add_object(obj)
+        return obj
