7d61a3175ffbe0ed881819dfae1b718755ca1ebe
Arefeh Yavary <yavary.rf@gmail.com>
2024-08-30 01:22:22 -0700

    Update planner.py

--- src/planner.py
+++ src/planner.py
@@ -1,1 +1,11 @@
 # Planning and solving functionalities
+from unified_planning.engines import OneshotPlanner
+
+class PlannerInterface:
+    def __init__(self, problem):
+        self.problem = problem
+
+    def solve(self, heuristic):
+        with OneshotPlanner(name='fmap', params={'heuristic': heuristic}) as planner:
+            result = planner.solve(self.problem)
+            return result
